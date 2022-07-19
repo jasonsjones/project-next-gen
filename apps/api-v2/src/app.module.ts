741802c180508e859fad80d8995f4d60dfc162e8
Jason Jones <jsjones96@gmail.com>
2022-07-19 13:57:48 -0700

    feat: add config service

--- apps/api-v2/src/app.module.ts
+++ apps/api-v2/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [PrismaModule, UsersModule, AuthModule],
+    imports: [ConfigModule.forRoot(), PrismaModule, UsersModule, AuthModule],
     controllers: [AppController],
     providers: [AppService]
 })
