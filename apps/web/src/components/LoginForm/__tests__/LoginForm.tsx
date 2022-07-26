afaa9af8e9f0a7313687b14e0277c4e6a1a672d2
Jason Jones <jsjones96@gmail.com>
2022-07-26 13:09:31 -0700

    feat: add msw to mock api calls for testing

--- apps/web/src/components/LoginForm/__tests__/LoginForm.tsx
+++ apps/web/src/components/LoginForm/__tests__/LoginForm.tsx
@@ -1,9 +1,23 @@
+import 'whatwg-fetch';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 import { cleanup, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import LoginForm from '../index';
 
+const server = setupServer(
+    rest.post('http://localhost:3000/api/v1/auth/login', (_, res, ctx) => {
+        return res(ctx.status(401), ctx.json({ statusCode: 401, message: 'Unauthorized' }));
+    })
+);
+
 const queryClient = new QueryClient();
+
 describe('LoginForm component', () => {
+    beforeAll(() => server.listen());
+    afterAll(() => server.close());
+
     afterEach(cleanup);
 
     it('renders a form', () => {
@@ -40,5 +54,23 @@ describe('LoginForm component', () => {
             </QueryClientProvider>
         );
         expect(screen.getByText('Login')).toBeTruthy();
+    });
+
+    it('displays unauthorized message when invalid credentials are provided', async () => {
+        render(
+            <QueryClientProvider client={queryClient}>
+                <LoginForm />
+            </QueryClientProvider>
+        );
+
+        const emailInput = screen.getByLabelText('Email');
+        const passwordInput = screen.getByLabelText('Password');
+        const loginButton = screen.getByText('Login');
+
+        await userEvent.type(emailInput, 'unknown-user@example.com');
+        await userEvent.type(passwordInput, '123456');
+        await userEvent.click(loginButton);
+
+        await screen.findByText(/unauthorized/i);
     });
 });
