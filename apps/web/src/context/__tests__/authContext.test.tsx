2f916ac12dbd47107c38d71214913245d2877c4e
Jason Jones <jsjones96@gmail.com>
2022-07-28 18:18:00 -0700

    feat: add logout function to auth context

--- apps/web/src/context/__tests__/authContext.test.tsx
+++ apps/web/src/context/__tests__/authContext.test.tsx
@@ -9,12 +9,13 @@ function wrapper({ children }: { children: React.ReactNode }) {
 }
 
 function AuthConsumer(): JSX.Element {
-    const { token, login } = useAuthContext();
+    const { token, login, logout } = useAuthContext();
 
     return (
         <>
             <p>Token value: {token}</p>
             <button onClick={() => login(TEST_TOKEN)}>Login</button>
+            <button onClick={() => logout()}>Logout</button>
         </>
     );
 }
@@ -29,5 +30,19 @@ describe('AuthContext', () => {
         const button = screen.getByText(/Login/);
         await userEvent.click(button);
         await screen.findByText(new RegExp(TEST_TOKEN));
+    });
+
+    it('clears the token value on logout', async () => {
+        render(<AuthConsumer />, { wrapper });
+        // Login and verify token
+        const loginBtn = screen.getByText(/Login/);
+        await userEvent.click(loginBtn);
+        await screen.findByText(new RegExp(TEST_TOKEN));
+
+        // Logout and verify token is removed
+        const logoutBtn = screen.getByText(/Logout/);
+        await userEvent.click(logoutBtn);
+        expect(await screen.findByText(/^Token/)).toHaveTextContent('Token value:');
+        expect(screen.queryByText(new RegExp(TEST_TOKEN))).toBeNull();
     });
 });
--- apps/web/src/context/authContext.tsx
+++ apps/web/src/context/authContext.tsx
@@ -3,11 +3,15 @@ import { createContext, useContext, useState } from 'react';
 interface AuthContextProps {
     token: string;
     login: (token: string) => void;
+    logout: () => void;
 }
 
 const initialState: AuthContextProps = {
     token: '',
     login: () => {
+        /* empty fn body */
+    },
+    logout: () => {
         /* empty fn body */
     }
 };
@@ -16,10 +20,16 @@ const AuthContext = createContext<AuthContextProps>(initialState);
 
 function AuthProvider({ children }: { children: React.ReactNode }): JSX.Element {
     const [token, setToken] = useState('');
+
     const login = (tokenVal: string) => {
         setToken(tokenVal);
     };
-    const value = { token, login };
+
+    const logout = () => {
+        setToken('');
+    };
+
+    const value = { token, login, logout };
     return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
