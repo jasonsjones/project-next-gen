36fbdb1857ff87d2083eec2db0926f7bd237bae0
Jason Jones <jsjones96@gmail.com>
2022-11-22 14:40:22 -0800

    refactor: change default avatar to dummyimage.com

--- apps/web/src/components/UserDetails/index.tsx
+++ apps/web/src/components/UserDetails/index.tsx
@@ -9,7 +9,7 @@ function UserDetails(): JSX.Element | null {
 
     return (
         <div className="flex gap-8 items-center">
-            <img src="https://randomuser.me/api/portraits/men/19.jpg" className="rounded-full" />
+            <img src="https://dummyimage.com/200/ccc/333" className="rounded-full" />
             <div>
                 <p className="text-4xl mb-2 text-purple-800">{`${data?.user.firstName} ${data?.user.lastName}`}</p>
                 <p className="text-2xl text-gray-700">{data?.user.email}</p>
