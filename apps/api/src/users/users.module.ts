7eae8ae0285e8371ae96c3f0e9fc83fbcf21f6cd
Jason Jones <jsjones96@gmail.com>
2022-12-12 17:26:59 -0800

    chore: remove unused imports; fix linting

--- apps/api/src/users/users.module.ts
+++ apps/api/src/users/users.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
-import { RolesGuard } from '../auth/roles.guard';
 import { PrismaModule } from '../prisma/prisma.module';
 import { UtilsModule } from '../utils/utils.module';
 import { UsersController } from './users.controller';
