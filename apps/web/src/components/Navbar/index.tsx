0a66ac2a44b7d691fecc614865825682a82950bb
Jason Jones <jsjones96@gmail.com>
2022-12-12 17:24:18 -0800

    feat: add custom site logo to navbar

--- apps/web/src/components/Navbar/index.tsx
+++ apps/web/src/components/Navbar/index.tsx
@@ -2,15 +2,16 @@ import { Link } from 'react-router-dom';
 import { useAuthContext } from '../../context/authContext';
 import AuthenticatedLinks from './AuthenticatedLinks';
 import UnauthenticatedLinks from './UnauthenticatedLinks';
+import siteLogo from '../../assets/logo-no-bg.svg';
 
 function Navbar() {
     const { token } = useAuthContext();
 
     return (
         <header className="text-slate-400 bg-slate-100">
-            <nav className="h-20 px-10  flex items-center">
+            <nav className="h-24 px-20 flex items-center">
                 <Link to="/">
-                    <span className="text-slate-600 text-xl">&lt;Site Logo&gt;</span>
+                    <img src={siteLogo} className="w-64" alt="Site logo" />
                 </Link>
                 {token ? <AuthenticatedLinks /> : <UnauthenticatedLinks />}
             </nav>
